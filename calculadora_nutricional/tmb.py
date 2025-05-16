049450465ec8901a28671709f36ed848f39f8f15
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2025-05-16 17:15:59 -0300

    feat: adicionando calculo tmb

---
+++ calculadora_nutricional/tmb.py
@@ -0,0 +1,27 @@
+
+# peso, altura, idade
+class TMB:
+    def __init__(self, weight: int, height: int, age: int):
+        self.weight = weight
+        self.height = height
+        self.age = age
+
+    def calc(self, gender):
+        if gender is None:
+            raise ValueError('A variavel gender tem que ser atribuido um valor female ou male')
+
+        if gender == 'female' or gender == 'f':
+            return self.female_calc()
+        return self.male_calc()
+
+    def female_calc(self) -> float:
+        result = 447.593 + (9.247 * float(self.weight)) + (3.098 * float(self.height)) - (4.330 * float(self.age))
+        return round(result)
+
+    def male_calc(self) -> float:
+        result = 88.362 + (13.397 * float(self.weight)) + (4.799 * float(self.height)) - (5.677 * float(self.age))
+        return round(result)
+
+
+example = TMB(72, 168, 22)
+print(example.calc('m'))
