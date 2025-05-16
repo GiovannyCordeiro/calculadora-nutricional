e1a7647c42ecee78c967a42b0a5871feda870bb1
Bianca Dias <biancakeyllaa@gmail.com>
2025-05-16 15:07:19 -0300

    feat: adiciona classificação da OMS ao cálculo de IMC

--- calculadora_nutricional/imc.py
+++ calculadora_nutricional/imc.py
@@ -1,5 +1,23 @@
 #Calculo do IMC( Indice de Massa corporal) Formula: peso divido pela altura ao quadrado. 
 def calcular_imc(peso, altura):
     imc = peso/altura**2
-    return imc
+    
+
+#Classificação da OMS (Organização Mundial de saúde de acordo com o IMC)
+    if imc < 18.5:
+        classificacao = "Abaixo do peso"
+    elif 18.5 <= imc < 25:
+        classificacao = "Peso normal"
+    elif 25 <= imc < 30:
+        classificacao = "Sobrepeso"
+    elif 30 <= imc < 35:
+        classificacao = "Obesidade grau 1"
+    elif 35 <= imc < 40:
+        classificacao = "Obesidade grau 2"
+    else:
+        classificacao = "Obesidade grau 3"
+
+    return imc, classificacao
+
+
 
