12073d88e24a4cc5cab67ed30c828a810b8e9965
Bianca Dias <biancakeyllaa@gmail.com>
2025-05-17 11:29:11 -0300

    fix: ajusta ordem das opções no menu da calculadora nutricional

--- calculadora_nutricional/main.py
+++ calculadora_nutricional/main.py
@@ -5,9 +5,9 @@ def menu():
     while True:
         print("\nCalculadora Nutriconal")
         print("1. Calcular IMC")
-        print("3. Calcular TMB")
-        print("4. Calcular GET")
-        print("2. Exibir Histórico de Pesos")
+        print("2. Calcular TMB")
+        print("3. Calcular GET")
+        print("4. Exibir Histórico de Pesos")
         print("5. Sair")
 
         opcao = input("Escolha o número correspondente a opção que deseja executar: ")
