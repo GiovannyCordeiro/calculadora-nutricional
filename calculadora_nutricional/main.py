1727a7cf658cfe503366d4a2951be97f81d68837
Bianca Dias <biancakeyllaa@gmail.com>
2025-05-17 11:05:35 -0300

    feat: atualiza menu principal e adiciona chamadas às funções TMB, calcular_imc, salvar_historico e exibir_historico

--- calculadora_nutricional/main.py
+++ calculadora_nutricional/main.py
@@ -1,1 +1,13 @@
- 
+from imc import calcular_imc, salvar_historico, exibir_historico
+from tmb import TMB
+
+def menu():
+    while True:
+        print("\nCalculadora Nutriconal")
+        print("1. Calcular IMC")
+        print("3. Calcular TMB")
+        print("4. Calcular GET")
+        print("2. Exibir Histórico de Pesos")
+        print("5. Sair")
+
+        opcao = input("Escolha o número correspondente a opção que deseja executar: ")
